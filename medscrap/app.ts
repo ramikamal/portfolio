3a07e6ab16bb9e3c5a867f64db526477cbba8388
Rami Kamal <ramikamal@outlook.com>
2019-09-17 11:15:05 +0400

    updated medscape package.json and tsconfig.json

---
+++ medscrap/app.ts
@@ -0,0 +1,24 @@
+import * as express from express;
+import * as robotsParser from robots-Parser;
+import * as mongoose from mongoose;
+import * as socketio from socketio;
+
+class Launch
+{
+    constructor()
+    {
+
+    }
+
+    public main()
+    {
+
+    }
+
+    public initialiseServer()
+    {
+
+    }
+
+    public singletonInstantion(){}
+}
